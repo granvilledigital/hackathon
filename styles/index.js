2cbd2b6e1dddbd91190fbededc88ae7151ea79e7
Guillaume Morin <gmorin.pro@gmail.com>
2016-10-29 22:52:43 +0200

    Split styles files

---
+++ styles/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import {StyleSheet, Platform} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+    },
+    info: {
+        position: 'absolute',
+        elevation: 10,
+        borderTopWidth: 1,
+        borderTopColor: '#f1f1f1',
+        backgroundColor: 'white',
+        left: 0,
+        right: 0,
+        bottom: 0
+    },
+    infoTitle: {
+        fontFamily: Platform.OS === 'ios' ? 'Helvetica' : 'sans-serif-light',
+        fontWeight: 'bold',
+        fontSize: 18,
+        padding: 5
+    },
+    infoName: {
+        fontFamily: Platform.OS === 'ios' ? 'Helvetica' : 'sans-serif-light',
+        fontSize: 16,
+        padding: 5
+    },
+    infoType: {
+        fontFamily: Platform.OS === 'ios' ? 'Helvetica' : 'sans-serif-light',
+        fontSize: 13,
+        padding: 5
+    },
+    infoLineContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    infoLine: {
+        fontFamily: Platform.OS === 'ios' ? 'Helvetica' : 'sans-serif-light',
+        fontSize: 16,
+        padding: 5
+    },
+    infoNumber: {
+        fontWeight: 'bold',
+        borderWidth: 3,
+        borderRadius: 15,
+        width: 30,
+        height: 30,
+        paddingTop: 5,
+        paddingLeft: 11
+
+    }
+
+});
+
+export default styles;
